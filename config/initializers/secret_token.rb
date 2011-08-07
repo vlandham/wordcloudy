f2ab7ed7e60c37a847c3a6225321c7621af38abd
Jim V <vlandham@gmail.com>
2011-08-07 12:12:32 -0500

    first init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wordcloudy::Application.config.secret_token = 'fd4d58574b8dabac5ddfd361ddc390730b8c3393f1f74f34c95c29d59cefca14367063f660c8490504f91a56300ac9eed269310d2ee4267f61a8df721ff34342'
